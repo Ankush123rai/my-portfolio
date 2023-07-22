bb9a5a72fc99160cedf28309bc4a989853bd7e12
Ankush123rai <ankush98rai@gamil.com>
2023-07-22 17:54:44 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
-import Home from './home/Home'
-import About from './about/About'
-import Navbar from './navbar/Navbar'
-import Education from './education/Education'
-import Project from './project/Project'
-import Contact from './contact/Contact'
-import MySection from './dashboard/MySection'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Education from "./components/education/Education";
+import Contact from "./pages/contact/Contact";
+import MySection from "./components/dashboard/MySection";
+import Project from "./pages/project/Project";
+import Navbar from "./components/navbar/Navbar";
 
 const App = () => {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/education" element={<Education/>}/>
-        <Route path="/project" element={<Project/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/mySection" element={<MySection/>}/>
-        <Route path="*" element={<Home/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/mySection" element={<MySection />} />
+        <Route path="*" element={<Home />} />
       </Routes>
     </Router>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
--- src/components/dashboard/MySection.jsx
+++ src/components/dashboard/MySection.jsx
@@ -1,13 +1,13 @@
 import Select from "react-select";
 import React, { useState } from "react";
 const MySection = () => {
-  const owner= localStorage.getItem('admin')
+  const owner = localStorage.getItem("admin");
   const options = [
     { value: "react", label: "React" },
     { value: "npm", label: "yarn" },
     { value: "yarn", label: "yarn" },
     { value: "angular", label: "Angular" },
-    { value: "Javascript", label: "JavaScript" }
+    { value: "Javascript", label: "JavaScript" },
   ];
   const [getData, setGetData] = useState([]);
   const [submitData, setSubmitData] = useState([]);
@@ -17,7 +17,7 @@ const MySection = () => {
     description: "",
     tags: [],
     github: "",
-    live: ""
+    live: "",
   });
 
   const handleMultiSelectChange = (selectedOptions) => {
@@ -28,35 +28,32 @@ const MySection = () => {
     }));
   };
 
-  const handleSubmitData = async(e) => {
+  const handleSubmitData = async (e) => {
     e.preventDefault();
-    const { projectImg, projectName, description, tags, github, live } = inputData;
+    const { projectImg, projectName, description, tags, github, live } =
+      inputData;
     const res = fetch("https://portfolio-a4c27-default-rtdb.firebaseio.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-      }, 
+      },
       body: JSON.stringify({
         projectImg: projectImg,
         projectName: projectName,
         description: description,
         tags: tags,
         github: github,
-        live: live
-      })}
-      )
+        live: live,
+      }),
+    });
 
-      if(res){
-        alert('data submitted')
-      }
-      else{
-        alert('data not submitted')
-      }
-
+    if (res) {
+      alert("data submitted");
+    } else {
+      alert("data not submitted");
+    }
   };
 
-
-  
   // console.log(getData);
   const getInputData = (e) => {
     const { name, value } = e.target;
@@ -71,32 +68,37 @@ const MySection = () => {
 
   return (
     <div>
-      {owner?
-      <div>
-        <form  method="POST" onSubmit={handleSubmitData}>
-        
-        <input type="file" name="projectImg" onChange={getInputData} />
-        <input type="text" name="projectName" onChange={getInputData} />
-        <textarea name="description" id="" cols="30" rows="10" onChange={getInputData} />
-        <Select
-          isMulti 
-          name="tags"
-          options={options}
-          className="basic-multi-select"
-          classNamePrefix="select"
-          onChange={handleMultiSelectChange}
-        />
-        <input type="url" name="github" onChange={getInputData} />
-        <input type="url" name="live" onChange={getInputData} />
+      {owner ? (
+        <div>
+          <form method="POST" onSubmit={handleSubmitData}>
+            <input type="file" name="projectImg" onChange={getInputData} />
+            <input type="text" name="projectName" onChange={getInputData} />
+            <textarea
+              name="description"
+              id=""
+              cols="30"
+              rows="10"
+              onChange={getInputData}
+            />
+            <Select
+              isMulti
+              name="tags"
+              options={options}
+              className="basic-multi-select"
+              classNamePrefix="select"
+              onChange={handleMultiSelectChange}
+            />
+            <input type="url" name="github" onChange={getInputData} />
+            <input type="url" name="live" onChange={getInputData} />
 
-        <button type="submit">Submit</button>
-
-        </form>
-      </div>:
-      <h1>get loss</h1>
-      }
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      ) : (
+        <h1>get loss</h1>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default MySection;
