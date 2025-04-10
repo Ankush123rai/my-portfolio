b4417685f84249f257d5af1f16f55127c473e3b2
ankush rai <ankush98rai@gmail.com>
2025-04-10 12:47:29 +0530

    updating porfolio

---
+++ README.md
@@ -0,0 +1,23 @@
+<div align="center">
+
+  <br />
+  
+  <img src="./public/readme-images/moj.png" />
+
+  <h2 align="center">Ankush Kumar Rai - Personal portfolio</h2>
+
+This website is fully responsive personal portfolio, <br />Responsive for all devices, built using Nextjs, TailwindCSS only.
+
+<!-- <a href="https://osamajavaid.vercel.app/"><strong>➥ Live Demo</strong></a> -->
+
+</div>
+
+<br />
+
+### Demo Screeshots
+
+
+
+### License
+
+This project is **free to use** and does not contains any license.
---
+++ constants/constants.js
@@ -0,0 +1,59 @@
+export const NAME = 'Ankush Kumar Rai';
+
+export const DESIGNATION = `MERN Stack Developer| Backend Developer | Open Source Contributor `;
+
+export const DETAILS = {
+  Residence: 'India',
+  City: 'Bangalore',
+  // Age: '23', dynamic age calculation
+  Age : new Date().getFullYear() - 2000,
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '95%',
+  },
+  {
+    title: 'Frontend Developer',
+    level: '91%',
+  },
+  {
+    title: 'Backend Developer',
+    level: '80%',
+  },
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'TypeScript',
+
+  'ReactJS',
+  'NextJS',
+  'React Native',
+  'Redux Toolkit',
+  'CSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'NodeJS',
+  'ExpressJS',
+  'REST API',
+  'Docker',
+  'MongoDB',
+  'Git',
+  'GitHub',
+  'Postman',
+];
+
+export const CONTACTS = {
+  EMAIL: 'ankush98rai@gmail.com',
+  PHONE: '+91 7002654128',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/Ankush123rai',
+  LINKEDIN: 'https://www.linkedin.com/in/ankush-rai-7733871b5/',
+  TWITTER: 'https://twitter.com',
+  FACEBOOK: 'https://www.facebook.com/',
+};
